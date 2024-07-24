fe4b2a89c301b85ed6c4750dab8a32c91a4c66ca
facundoacostayl <facundoacostayl@outlook.com>
2024-07-24 13:40:20 -0300

    Update fe dashboard role access

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,13 @@
 import { useQuery } from 'react-query';
 
 import BarChart from '../components/dashboard/BarChart';
-import UpdateProfile from '../components/dashboard/UpdateProfile';
 import Layout from '../components/layout';
-import courseService from '../services/CourseService';
+import useAuth from '../hooks/useAuth';
 import statsService from '../services/StatsService';
 
 export default function Dashboard() {
   const { data, isLoading } = useQuery('stats', statsService.getStats);
+  const { authenticatedUser } = useAuth();
 
   return (
     <Layout>
@@ -16,23 +16,30 @@ export default function Dashboard() {
       <div className="mt-5 flex flex-col gap-5">
         {!isLoading ? (
           <div className="flex flex-col sm:flex-row gap-5">
-            <div className="card shadow text-white bg-gray-600 flex-1">
-              <h1 className="font-semibold sm:text-4xl text-center mb-3">
-                {data.numberOfUsers}
-              </h1>
-              <p className="text-center sm:text-lg font-semibold">Users</p>
-            </div>
+            {authenticatedUser.role === 'admin' && (
+              <div className="card shadow text-white bg-gray-600 flex-1">
+                <h1 className="font-semibold sm:text-4xl text-center mb-3">
+                  {data.numberOfUsers}
+                </h1>
+                <p className="text-center sm:text-lg font-semibold">Users</p>
+              </div>
+            )}
+
             <div className="card shadow text-white bg-[#d95054] flex-1">
               <h1 className="font-semibold sm:text-4xl mb-3 text-center">
                 {data.numberOfCourses}
               </h1>
-              <p className="text-center sm:text-lg font-semibold">Courses</p>
+              <p className="text-center sm:text-lg font-semibold">
+                Courses Available
+              </p>
             </div>
             <div className="card shadow text-white bg-[#dd5f5f] flex-1">
               <h1 className="font-semibold sm:text-4xl mb-3 text-center">
                 {data.numberOfContents}
               </h1>
-              <p className="text-center sm:text-lg font-semibold">Contents</p>
+              <p className="text-center sm:text-lg font-semibold">
+                Contents Uploaded
+              </p>
             </div>
           </div>
         ) : null}
